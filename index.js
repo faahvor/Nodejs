7819d414c0ab2c096b129ea9c63ebeaf80ccf7ac
Favour Ifurukpe <tinafavour2018@gmail.com>
2024-06-27 12:16:30 +0100

    my server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 http.createServer((req, res)=>{
 res.writeHead(200, {"Content-Type:": "text/html"})
-res.write("<h1>Hello world</h1>")
+res.write("<h1>Hello favour</h1>")
 res.removeHeader();
 }).listen(3000)
 console.log("server is running on  http://local.3000");
